72915cb8083002836a035866fa172c61df4b824a
devoc09 <b1816112@planet.kanazawa-it.ac.jp>
2020-10-15 04:30:45 +0900

    first commit

---
+++ colors.js
@@ -0,0 +1,18 @@
+// This file can be used with the themer CLI, see https://github.com/mjswensen/themer
+
+module.exports.colors = {
+  "dark": {
+    "shade0": "#1C2036",
+    "shade7": "#B2FFFF",
+    "accent0": "#FF2400",
+    "accent1": "#0074D5",
+    "accent2": "#B5222F",
+    "accent3": "#FFFFAA",
+    "accent4": "#44EE3E",
+    "accent5": "#00A9FF",
+    "accent6": "#7F8CFF",
+    "accent7": "#00FFFF"
+  }
+};
+
+// Your theme's URL: https://themer.dev/?colors.dark.shade0=%231c2036&colors.dark.shade7=%23b2ffff&colors.dark.accent0=%23ff2400&colors.dark.accent1=%230074d5&colors.dark.accent2=%23b5222f&colors.dark.accent3=%23ffffaa&colors.dark.accent4=%2344ee3e&colors.dark.accent5=%2300a9ff&colors.dark.accent6=%237f8cff&colors.dark.accent7=%2300ffff&activeColorSet=dark
